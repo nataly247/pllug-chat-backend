6bc83cc3017d66cb8ce330ff969be7a79d1c04eb
yevgeniy <yevgeniy.kit@indeema.com>
2019-03-18 16:54:03 +0200

    + update socket.io to use native http server at the same port as a project

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const UserRouter = require("./src/user");
 const MessageRouter = require("./src/message");
-const io = require("./src/socket");
-
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use(passport.initialize());
-
 app.use("/user", UserRouter);
-app.use("/message", MessageRouter);
 
+app.use("/message", MessageRouter);
 app.use((err, req, res, next) => {
   console.error(err);
   res
     .status(err.status || 500)
     .json({ message: err.message || "Internal server error" });
 });
+const server = app.listen(process.env.PORT || 3000);
 
-app.listen(process.env.PORT || 3000);
+const io = require("./src/socket")(server);
--- src/socket.js
+++ src/socket.js
@@ -1,60 +1,60 @@
 const userController = require("./user/user.controller");
 const messsageController = require("./message/message.controller");
-const io = require("socket.io")(1337);
 
 const jwt = require("jsonwebtoken");
 const authConfig = require("../config/auth");
 
-io.use((socket, next) => {
-  const { token } = socket.handshake.query;
-  try {
-    jwt.verify(token, authConfig.authSecret);
-  } catch (e) {
-    return next("Authentication error");
-  }
-  const { user } = jwt.decode(token);
-  if (!user._id) {
-    return next("Authentication error");
-  }
-  return userController.findById(user._id).then(user => {
-    if (!user) {
-      return next("User not found");
+module.exports = server => {
+  const io = require("socket.io")(server);
+  io.use((socket, next) => {
+    const { token } = socket.handshake.query;
+    try {
+      jwt.verify(token, authConfig.authSecret);
+    } catch (e) {
+      return next("Authentication error");
     }
-    socket.user = user;
-    return next();
+    const { user } = jwt.decode(token);
+    if (!user._id) {
+      return next("Authentication error");
+    }
+    return userController.findById(user._id).then(user => {
+      if (!user) {
+        return next("User not found");
+      }
+      socket.user = user;
+      return next();
+    });
   });
-});
 
-const emitMessage = message => io.emit("message", message);
+  const emitMessage = message => io.emit("message", message);
 
-io.on("connection", socket => {
-  messsageController
-    .createMessage({
-      user: socket.user._id,
-      text: `User ${socket.user.username} connected`
-    })
-    .then(emitMessage);
-
-  socket.on("message", text =>
+  io.on("connection", socket => {
     messsageController
       .createMessage({
         user: socket.user._id,
-        text,
-        type: "user"
-      })
-      .then(emitMessage)
-  );
-
-  socket.on("disconnect", () => {
-    messsageController
-      .createMessage({
-        user: socket.user._id,
-        text: `User ${socket.user.username} disconnected`
+        text: `User ${socket.user.username} connected`
       })
       .then(emitMessage);
 
-    socket.removeAllListeners("message");
-  });
-});
+    socket.on("message", text =>
+      messsageController
+        .createMessage({
+          user: socket.user._id,
+          text,
+          type: "user"
+        })
+        .then(emitMessage)
+    );
 
-module.exports = io;
+    socket.on("disconnect", () => {
+      messsageController
+        .createMessage({
+          user: socket.user._id,
+          text: `User ${socket.user.username} disconnected`
+        })
+        .then(emitMessage);
+
+      socket.removeAllListeners("message");
+    });
+  });
+};
