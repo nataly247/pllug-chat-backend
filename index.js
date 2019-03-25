f0d0ae14a09cb79a1913b1a65a42603c26f89374
yevgeniy <yevgeniy.kit@indeema.com>
2019-03-25 12:27:11 +0200

    + Access-Control-Allow headers

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const passport = require("passport");
 const mainRouter = require("./src/router");
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
