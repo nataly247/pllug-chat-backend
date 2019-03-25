53541e659881c177c1b92ed5e2ce687cc65d40f3
yevgeniy <yevgeniy.kit@indeema.com>
2019-03-25 14:24:42 +0200

    + cors headers

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
-  // res.header("Access-Control-Expose-Headers", true);
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
